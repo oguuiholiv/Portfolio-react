2411727dd452aa69f70c38dd84528e5d2a50d837
Guilherme Oliveira <francoguilherme.sec@gmail.com>
2022-12-27 13:47:50 -0300

    chore: componentes criados

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  
+`;
+
+export const Header = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
